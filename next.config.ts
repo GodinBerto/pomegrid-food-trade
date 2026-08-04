b22fe23820df171e66232b5f96b93aa8bb6ceb99
GodinBerto <godfredquarm123@gmail.com>
2026-08-04 03:38:07 +0000

    feat: update API destination URLs to production server in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,15 +5,15 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/v1/api/:path*",
-        destination: "http://localhost:8000/api/v1/:path*", // Proxy to Backend
+        destination: "http://pomegrid.pythonanywhere.com/api/v1/:path*", // Proxy to Backend
       },
       {
         source: "/api/v1/:path*",
-        destination: "http://localhost:8000/api/v1/:path*", // Fallback proxy
+        destination: "http://pomegrid.pythonanywhere.com/api/v1/:path*", // Fallback proxy
       },
       {
         source: "/uploads/:path*",
-        destination: "http://localhost:8000/uploads/:path*",
+        destination: "http://pomegrid.pythonanywhere.com/uploads/:path*",
       },
     ];
   },
